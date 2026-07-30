f56303044cc3ddbce4d3921325bc3eaed73f477c
AmirFaridi-2002 <107439717+AmirFaridi-2002@users.noreply.github.com>
2026-07-30 04:37:48 +0000

    Deploying to gh-pages from @ AmirFaridi-2002/AmirFaridi-2002.github.io@83d95581528b76b5bf760bd7ba38b15bf0536b79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,26 +51,146 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/tom-bombadil/";
           },
-        },{id: "books-the-midnight-library",
-          title: 'The Midnight Library',
+        },{id: "books-atomic-habits",
+          title: 'Atomic Habits',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/The_Midnight_Library/";
-            },},{id: "books-regarding-the-pain-of-others",
-          title: 'Regarding the Pain of Others',
+              window.location.href = "/books/Atomic_Habits/";
+            },},{id: "books-beren-and-lúthien",
+          title: 'Beren and Lúthien',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/Regarding_The_Pain_Of_Others/";
-            },},{id: "books-when-nietzsche-wept",
-          title: 'When Nietzsche Wept',
+              window.location.href = "/books/Beren_And_Luthien/";
+            },},{id: "books-for-the-win",
+          title: 'For the Win',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/When_Nietzsche_Wept/";
+              window.location.href = "/books/For_The_Win/";
+            },},{id: "books-hour-of-the-heart",
+          title: 'Hour of the Heart',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Hour_Of_The_Heart/";
             },},{id: "books-man-39-s-search-for-meaning",
           title: 'Man&amp;#39;s Search for Meaning',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/Mans_Search_For_Meaning/";
+            },},{id: "books-momma-amp-the-meaning-of-life",
+          title: 'Momma &amp;amp; the Meaning of Life',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Momma_And_The_Meaning_Of_Lfe/";
+            },},{id: "books-regarding-the-pain-of-others",
+          title: 'Regarding the Pain of Others',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Regarding_The_Pain_Of_Others/";
+            },},{id: "books-staring-at-the-sun",
+          title: 'Staring at the Sun',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Staring_At_The_Sun/";
+            },},{id: "books-the-art-of-loving",
+          title: 'The Art of Loving',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Art_Of_Loving/";
+            },},{id: "books-the-birth-of-tragedy",
+          title: 'The Birth of Tragedy',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Birth_Of_Tragedy/";
+            },},{id: "books-the-children-of-húrin",
+          title: 'The Children of Húrin',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Children_Of_Hurin/";
+            },},{id: "books-the-complete-history-of-middle-earth",
+          title: 'The Complete History of Middle-earth',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Complete_History_Of_Middle_Earth/";
+            },},{id: "books-the-death-of-ivan-ilyich",
+          title: 'The Death of Ivan Ilyich',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Death_Of_Ivan_Ilyich/";
+            },},{id: "books-the-fall-of-gondolin",
+          title: 'The Fall of Gondolin',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Fall_Of_Gondolin/";
+            },},{id: "books-the-fellowship-of-the-ring",
+          title: 'The Fellowship of the Ring',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Fellowship_Of_The_Ring/";
+            },},{id: "books-the-gift-of-therapy",
+          title: 'The Gift of Therapy',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Gift_Of_Therapy/";
+            },},{id: "books-the-hobbit",
+          title: 'The Hobbit',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Hobbit/";
+            },},{id: "books-the-midnight-library",
+          title: 'The Midnight Library',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Midnight_Library/";
+            },},{id: "books-the-power-of-habit",
+          title: 'The Power of Habit',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Power_Of_Habit/";
+            },},{id: "books-the-return-of-the-king",
+          title: 'The Return of the King',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Return_Of_The_King/";
+            },},{id: "books-the-schopenhauer-cure",
+          title: 'The Schopenhauer Cure',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Schopenhauer_Cure/";
+            },},{id: "books-the-silmarillion",
+          title: 'The Silmarillion',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Silmarillion/";
+            },},{id: "books-the-story-of-civilization",
+          title: 'The Story of Civilization',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Story_Of_Civilization/";
+            },},{id: "books-the-two-towers",
+          title: 'The Two Towers',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/The_Two_Towers/";
+            },},{id: "books-when-nietzsche-wept",
+          title: 'When Nietzsche Wept',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/When_Nietzsche_Wept/";
+            },},{id: "books-thus-spoke-zarathustra",
+          title: 'Thus Spoke Zarathustra',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Thus_Spoke_Zarathustra/";
+            },},{id: "books-unfinished-tales-of-númenor-and-middle-earth",
+          title: 'Unfinished Tales of Númenor and Middle-earth',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Unfinished_Tales_Of_Numenor_And_Middle_Earth/";
+            },},{id: "books-walking-with-plato",
+          title: 'Walking with Plato',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Walking_With_Plato/";
             },},{id: "projects-correctness-and-incorrectness-reasoning-for-quantum-programs",
           title: 'Correctness and Incorrectness Reasoning for Quantum Programs',
           description: "BSC Thesis Report (University of Tehran)",
